23abb170f9251116af15c12a20d92d86ee1ee7bd
dbucksteincc <dbucksteincc>
2017-01-15 02:22:40 -0500

    Added solution and project files. Added all base source.

---
+++ EGP300 Graphics2 FW/source/egpfw/egpfwShaderProgram.c
@@ -0,0 +1,117 @@
+// By Dan Buckstein
+// Modified by: _______________________________________________________________
+#include "egpfw/egpfw/egpfwShaderProgram.h"
+
+
+#include "GL/glew.h"
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// shader type array
+const unsigned int egpfwShaderType[] =
+{
+	0,
+	GL_VERTEX_SHADER,
+	GL_TESS_CONTROL_SHADER,
+	GL_TESS_EVALUATION_SHADER,
+	GL_GEOMETRY_SHADER,
+	GL_FRAGMENT_SHADER,
+};
+
+
+//-----------------------------------------------------------------------------
+// shader functions
+
+egpShader egpfwCreateShaderFromSource(const egpShaderType type, const char *source)
+{
+	// this function is complete!
+	return egpfwCreateShaderFromSources(type, &source, 1);
+}
+
+egpShader egpfwCreateShaderFromSources(const egpShaderType type, const char **sources, unsigned int numSources)
+{
+	egpShader ret = { 0 };
+	//...
+	return ret;
+}
+
+int egpfwReleaseShader(egpShader *shader)
+{
+	//...
+	return 0;
+}
+
+
+//-----------------------------------------------------------------------------
+// program functions
+
+egpProgram egpfwCreateProgram()
+{
+	egpProgram ret = { 0 };
+	//...
+	return ret;
+}
+
+int egpfwAttachShaderToProgram(egpProgram *program, const egpShader *shader)
+{
+	//...
+	return 0;
+}
+
+int egpfwDetachShaderFromProgram(egpProgram *program, const egpShaderType type)
+{
+	//...
+	return 0;
+}
+
+int egpfwLinkProgram(egpProgram *program)
+{
+	//...
+	return 0;
+}
+
+int egpfwValidateProgram(egpProgram *program)
+{
+	//...
+	return 0;
+}
+
+void egpfwActivateProgram(const egpProgram *program)
+{
+	//...
+}
+
+int egpfwReleaseProgram(egpProgram *program)
+{
+	//...
+	return 0;
+}
+
+int egpfwGetUniformLocation(const egpProgram *program, const char *uniformName)
+{
+	//...
+	return -1;
+}
+
+void egpfwSendUniformInt(int location, const egpUniformIntType type, const unsigned int count, const int *values)
+{
+	//...
+}
+
+void egpfwSendUniformFloat(int location, const egpUniformFloatType type, const unsigned int count, const float *values)
+{
+	//...
+}
+
+void egpfwSendUniformFloatMatrix(int location, const egpUniformFloatMatrixType type, const unsigned int count, const int transpose, const float *values)
+{
+	//...
+}
+
+
+//-----------------------------------------------------------------------------
